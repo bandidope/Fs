69f7c4348c3bbd451a65bafc79947333a6ddab44
DevYerZx <yersonorosco2002@gmail.com>
2026-03-08 13:15:16 -0500

    Update prueba.js

--- commands/descargas/prueba.js
+++ commands/descargas/prueba.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-import yts from "yt-search";
+import axios from "axios"
+import yts from "yt-search"
 
-const API = "https://0f80537128db9987-201-230-121-168.serveousercontent.com/ytmp4";
+const API = "https://0f66da8bd81e5d32-201-230-121-168.serveousercontent.com/ytmp3"
 
-const channelInfo = global.channelInfo || {};
+const channelInfo = global.channelInfo || {}
 
 function safeFileName(name){
 return String(name||"audio")
@@ -18,68 +18,67 @@ category:"descarga",
 
 run: async(ctx)=>{
 
-const {sock,from,args} = ctx;
-const msg = ctx.m || ctx.msg;
+const {sock,from,args} = ctx
+const msg = ctx.m || ctx.msg
 
 if(!args.length){
 return sock.sendMessage(from,{
 text:"❌ Uso: .ytmp3yer canción",
 ...channelInfo
-});
+})
 }
 
 try{
 
-const query = args.join(" ");
-const search = await yts(query);
-const video = search.videos[0];
+const query = args.join(" ")
+const search = await yts(query)
+const video = search.videos[0]
 
 if(!video){
 return sock.sendMessage(from,{
 text:"❌ No encontré resultados",
 ...channelInfo
-});
+})
 }
 
 await sock.sendMessage(from,{
 image:{url:video.thumbnail},
-caption:`🎵 Descargando...\n\n${video.title}`,
+caption:`🎵 Descargando MP3...\n\n${video.title}`,
 ...channelInfo
-},{quoted:msg});
+},{quoted:msg})
 
 const {data} = await axios.get(API,{
-params:{url:video.url}
-});
+params:{url:video.url},
+timeout:20000
+})
 
-if(!data?.download) throw new Error("API sin audio");
+if(!data?.download) throw "sin mp3"
 
-const audioUrl = data.download;
-
-const audioBuffer = await axios.get(audioUrl,{
+const audio = await axios.get(data.download,{
 responseType:"arraybuffer",
 headers:{
 "User-Agent":"Mozilla/5.0"
 }
-});
+})
 
 await sock.sendMessage(from,{
-audio: audioBuffer.data,
+audio:audio.data,
 mimetype:"audio/mpeg",
 fileName:safeFileName(video.title)+".mp3",
 ...channelInfo
-},{quoted:msg});
+},{quoted:msg})
 
-}catch(err){
+}catch(e){
 
-console.log("YTMP3YER ERROR:",err);
+console.log("MP3 ERROR:",e)
 
 sock.sendMessage(from,{
-text:"❌ Error descargando música",
+text:"❌ Error descargando mp3",
 ...channelInfo
-});
+})
 
 }
 
 }
 
-};
+}
